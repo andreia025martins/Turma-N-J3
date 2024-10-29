37ec89a20ee7b382424cfd2e29bce1f63c58ac35
Andréia <andreia.martins17@escola.pr.gov.br>
2024-10-29 20:46:00 -0300

    Javascript inicio

---
+++ perguntas.js
@@ -0,0 +1,10 @@
+criarCartao(
+    'Programação',
+    'O que é JavaScript',
+    'É uma linguagem de programação'
+)
+criarCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
