39eee9f8fddfb2b10a169bb7013c0c31a1dafd24
Andréia <andreia.martins17@escola.pr.gov.br>
2024-11-05 21:37:24 -0300

    Ajustes reunião 6

--- perguntas.js
+++ perguntas.js
@@ -1,9 +1,9 @@
-criarCartao(
+criaCartao(
     'Programação',
     'O que é JavaScript',
     'É uma linguagem de programação'
 )
-criarCartao(
+criaCartao(
     'Geografia',
     'Qual a capital da França',
     'A capital da França é Paris'
